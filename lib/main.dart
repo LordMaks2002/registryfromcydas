df815ea0eda3758222febe3a99079c3612443c53
LordMaks2002 <124434358+LordMaks2002@users.noreply.github.com>
2025-03-19 21:40:21 +0200

    addinfo

--- lib/main.dart
+++ lib/main.dart
@@ -36,92 +36,105 @@ class _MainAppState extends State<MainApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
-          child: Center(
-            child: Container(
-              width: 500,
-              height: 500,
-              color: Colors.grey,
+        body: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
               child: Center(
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 12,
+                child: Container(
+                  width: 500,
+                  height: 500,
+                  color: Colors.grey,
+                  child: Center(
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 12,
+                        ),
+                        TextField(
+                          controller: _controller,
+                          keyboardType: TextInputType.emailAddress,
+                          textAlign: TextAlign.end,
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.black,
+                              hintText: 'Enter your email',
+                              hintStyle: TextStyle(color: Colors.white),
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius: BorderRadius.circular(12),
+                              )),
+                          onChanged: (text) {
+                            setState(() {});
+                          },
+                        ),
+                        SizedBox(
+                          height: 12,
+                        ),
+                        TextField(
+                          controller: _controller1,
+                          keyboardType: TextInputType.emailAddress,
+                          textAlign: TextAlign.end,
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.black,
+                              hintText: 'Enter your password',
+                              hintStyle: TextStyle(color: Colors.white),
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius: BorderRadius.circular(12),
+                              )),
+                          onChanged: (text) {
+                            setState(() {});
+                          },
+                        ),
+                        SizedBox(
+                          height: 18,
+                        ),
+                        ElevatedButton(
+                          onPressed: stateButton()
+                              ? () {
+                                  submitActive();
+                                }
+                              : null,
+                          style: TextButton.styleFrom(
+                            backgroundColor:
+                                colorChange ? Colors.green : Colors.grey,
+                            side: BorderSide(
+                                color: _controller.text.isNotEmpty &&
+                                        _controller1.text.isNotEmpty
+                                    ? Color.fromARGB(255, 255, 255, 255)
+                                    : Color.fromARGB(2, 0, 0, 0)),
+                          ),
+                          child: Text('Submit'),
+                        ),
+                        SizedBox(
+                          height: 18,
+                        ),
+                        Text(
+                          registrationMessage,
+                          style: TextStyle(
+                              color: const Color.fromARGB(255, 0, 255, 8),
+                              fontSize: 16),
+                        ),
+                      ],
                     ),
-                    TextField(
-                      controller: _controller,
-                      keyboardType: TextInputType.emailAddress,
-                      textAlign: TextAlign.end,
-                      style: TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.black,
-                          hintText: 'Enter your email',
-                          hintStyle: TextStyle(color: Colors.white),
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                            borderRadius: BorderRadius.circular(12),
-                          )),
-                      onChanged: (text) {
-                        setState(() {});
-                      },
-                    ),
-                    SizedBox(
-                      height: 12,
-                    ),
-                    TextField(
-                      controller: _controller1,
-                      keyboardType: TextInputType.emailAddress,
-                      textAlign: TextAlign.end,
-                      style: TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.black,
-                          hintText: 'Enter your password',
-                          hintStyle: TextStyle(color: Colors.white),
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                            borderRadius: BorderRadius.circular(12),
-                          )),
-                      onChanged: (text) {
-                        setState(() {});
-                      },
-                    ),
-                    SizedBox(
-                      height: 18,
-                    ),
-                    ElevatedButton(
-                      onPressed: stateButton()
-                          ? () {
-                              submitActive();
-                            }
-                          : null,
-                      style: TextButton.styleFrom(
-                        backgroundColor:
-                            colorChange ? Colors.green : Colors.grey,
-                        side: BorderSide(
-                            color: _controller.text.isNotEmpty &&
-                                    _controller1.text.isNotEmpty
-                                ? Color.fromARGB(255, 255, 255, 255)
-                                : Color.fromARGB(2, 0, 0, 0)),
-                      ),
-                      child: Text('Submit'),
-                    ),
-                    SizedBox(
-                      height: 18,
-                    ),
-                    Text(
-                      registrationMessage,
-                      style: TextStyle(
-                          color: const Color.fromARGB(255, 0, 255, 8),
-                          fontSize: 16),
-                    ),
-                  ],
+                  ),
                 ),
               ),
             ),
-          ),
+            Positioned(
+              bottom: 10,
+              right: 10,
+              child: Text(
+                'Email: $currentEmail\nPassword: $currentPassword',
+                style: TextStyle(color: Colors.black, fontSize: 12),
+                textAlign: TextAlign.right,
+              ),
+            ),
+          ],
         ),
       ),
     );
